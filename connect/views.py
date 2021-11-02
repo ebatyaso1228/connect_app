b3eed00d87f7204d587af8fd56f93bcc1501733c
ebatyaso1228 <86719773+ebatyaso1228@users.noreply.github.com>
2021-11-03 01:51:04 +0900

    add loginpage&signuppage
    add loginpage&signuppage

--- connect/views.py
+++ connect/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView , DetailView , CreateView , DeleteView , UpdateView
 from .models import ConnectModel
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -29,5 +32,28 @@ class ConnectUpdate(UpdateView):
     model = ConnectModel
     fields = ('title', 'memo')
     success_url = reverse_lazy('list')
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username,'',password)
+        return render(request, 'signup.html', {'some':100})
+
+    return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    
+    return render(request, 'login.html')
+
 
     
