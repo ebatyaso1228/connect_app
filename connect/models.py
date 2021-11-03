4d950f1009d6db4999108b7e93b64e3bbe1a4feb
ebatyaso1228 <86719773+ebatyaso1228@users.noreply.github.com>
2021-11-03 23:52:32 +0900

    Add_imagemodels
    Add_imagemodels

--- connect/models.py
+++ connect/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ConnectModel(models.Model):
+    myimage = models.ImageField()
     title = models.CharField(max_length=100)
     memo = models.TextField()
     def __str__(self):
