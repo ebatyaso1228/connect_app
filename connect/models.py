8db6ecbbb025092989994cabc316fbb27bbad22e
ebatyaso1228 <86719773+ebatyaso1228@users.noreply.github.com>
2021-11-06 02:00:49 +0900

    level_count
    add level

--- connect/models.py
+++ connect/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.core.validators import validate_integer
+
 
 # Create your models here.
 class ConnectModel(models.Model):
     myimage = models.ImageField()
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    level = models.IntegerField(
+        verbose_name='',
+        blank=True,
+        null=True,
+        default=0,
+  
+    )
+
+
+
+
     def __str__(self):
         return self.title
 
